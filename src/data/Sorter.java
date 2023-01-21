53a67e8fbc829732942cfc3261adef4e1b3a3304
shan <shan2005an@gmail.com>
2023-01-21 10:35:47 -0500

    mergesort methods

--- src/data/Sorter.java
+++ src/data/Sorter.java
@@ -51,4 +51,62 @@ public class Sorter {
         this.sorter();
         return sortingArray;
     }
+
+   
+
+    public void sorter() {
+       DataPoint[] temp = new DataPoint[sortingArray.size()];
+       mergeSortHelper(0, sortingArray.size() - 1, temp);
+   }
+  
+   private void mergeSortHelper(int from, int to, DataPoint[] temp) {
+       if(to - from >= 1) {
+           int mid = (from + to) / 2;   // split the list in half
+           mergeSortHelper(from, mid, temp);  //mergesort the first half
+           mergeSortHelper(mid + 1, to, temp); // mergesort the second half
+           merge(from, mid, to, temp); //merge
+       }
+   }
+  
+   private void merge(int from, int mid, int to, DataPoint[] temp) {
+       int i = from;       // track left array position
+       int j = mid + 1;    // track right array position
+       int k = from;       // track temp position
+      
+       while(i <= mid && j <= to) {
+           if(getValue(sortingArray.get(i)) < getValue(sortingArray.get(i))) {
+               temp[k] = sortingArray.get(i);
+               i++;
+
+           } else {
+               temp[k] = sortingArray.get(j);
+               j++;
+
+           }
+           
+           k++;
+       }
+      
+       // We may left over elements from either list
+       while(i <= mid)
+       {
+           temp[k] = sortingArray.get(i);
+           i++;
+           k++;
+       }
+      
+       while(j <= to)
+       {
+           temp[k] = sortingArray.get(j);
+           j++;
+           k++;
+       }
+      
+       // Copy over elements from temp to arr
+       for(k = from; k <= to; k++)
+       {
+           sortingArray.add(k, temp[k]);
+       }
+   }
 }
+
